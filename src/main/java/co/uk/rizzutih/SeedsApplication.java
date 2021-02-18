7921a9c6408d1b656bdcaa6e7fde34857eb31dbe
Hernan Rizzuti <hernanrizzuti@gmail.com>
2021-02-18 20:42:37 +0000

    first commit

---
+++ src/main/java/co/uk/rizzutih/SeedsApplication.java
@@ -0,0 +1,13 @@
+package co.uk.rizzutih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/uk/rizzutih/SeedsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.rizzutih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
